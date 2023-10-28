9a7c30149a1967b4c4c88691df0c12705e75a89f
Ivan Garcia <ivanggv2@gmail.com>
2023-10-28 17:30:05 +0200

    added reducers to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import type { PreloadedState } from "@reduxjs/toolkit";
 import { dataReducer } from "./slices/dataSlice/dataSlice";
+import { uiDataReducer } from "./slices/uiSlice/uiSlice";
 
 const rootReducer = combineReducers({
   data: dataReducer,
+  ui: uiDataReducer,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
