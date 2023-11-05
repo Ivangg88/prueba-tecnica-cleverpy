a64543c698ef3e434c0a367e4dfcdf353ab38a51
Ivan Garcia <ivanggv2@gmail.com>
2023-11-06 00:13:43 +0100

    added to component Header the function for logout

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -5,21 +5,6 @@ import { RootState } from "../../redux/store";
 import { screen } from "@testing-library/react";
 
 describe("Given a component Header", () => {
-  describe("When is rendered with a unlogged user", () => {
-    test("Then it should show a link with the text:'Login", () => {
-      const preloadStore: PreloadedState<RootState> = {
-        user: { isLogged: false, token: "", userName: "Mock user" },
-      };
-      const expectedLinkName = "Login";
-
-      renderWithProviders(<Header />, { preloadedState: preloadStore });
-
-      const receivedLink = screen.getByRole("link", { name: expectedLinkName });
-
-      expect(receivedLink).toBeInTheDocument();
-    });
-  });
-
   describe("When is rendered with a logged user", () => {
     test("Then it should show a span with the text: 'Welcome mockUser and a link qith the text:'Logout", () => {
       const preloadStore: PreloadedState<RootState> = {
